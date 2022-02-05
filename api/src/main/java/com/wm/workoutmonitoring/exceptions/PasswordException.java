619e8d792b3eb539669d67f92103c1ac889f6193
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-05 21:07:29 +0100

    feat: added PasswordException
    Added PasswordException. This exception is used in the PasswordService
    class. It is thrown when the service wasn't able to hash a password.

---
+++ api/src/main/java/com/wm/workoutmonitoring/exceptions/PasswordException.java
@@ -0,0 +1,7 @@
+package com.wm.workoutmonitoring.exceptions;
+
+public class PasswordException extends Exception{
+    public PasswordException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+}

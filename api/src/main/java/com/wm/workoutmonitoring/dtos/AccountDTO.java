56b5969bfbe20270442959b05e47b3e70b75ab00
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-05 21:06:19 +0100

    feat: added dtos package
    Added DTO classes for Account, Exercise & Workout. These classes will be
    used in the controllers.

---
+++ api/src/main/java/com/wm/workoutmonitoring/dtos/AccountDTO.java
@@ -0,0 +1,51 @@
+package com.wm.workoutmonitoring.dtos;
+
+import java.util.Date;
+
+public class AccountDTO {
+    private String name;
+    private String password;
+    private String email;
+    private Date dateOfBirth;
+    private String gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ api/src/main/java/com/wm/workoutmonitoring/dtos/ExerciseDTO.java
@@ -0,0 +1,67 @@
+package com.wm.workoutmonitoring.dtos;
+
+public class ExerciseDTO {
+    private String name;
+    private String description;
+    private int sets;
+    private int reps;
+    private double weight;
+    private double rpe;
+    private String baseExercise;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSets() {
+        return sets;
+    }
+
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+
+    public int getReps() {
+        return reps;
+    }
+
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getRpe() {
+        return rpe;
+    }
+
+    public void setRpe(double rpe) {
+        this.rpe = rpe;
+    }
+
+    public String getBaseExercise() {
+        return baseExercise;
+    }
+
+    public void setBaseExercise(String baseExercise) {
+        this.baseExercise = baseExercise;
+    }
+}

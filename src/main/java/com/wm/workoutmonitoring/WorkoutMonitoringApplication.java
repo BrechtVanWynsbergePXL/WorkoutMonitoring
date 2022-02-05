bc1ad78d79bd41bde351b43d0e0ca363e02f2638
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-05 17:21:35 +0100

    inital commit

---
+++ src/main/java/com/wm/workoutmonitoring/WorkoutMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.wm.workoutmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wm/workoutmonitoring/WorkoutMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wm.workoutmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

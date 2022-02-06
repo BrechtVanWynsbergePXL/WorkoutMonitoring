7609b3ecf4fe8defade678acd81aa1de256a4143
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-06 18:08:14 +0100

    feat: added GenderException class
    This exception will be thrown when a gender string can't be converted to
    a gender from the gender enum.

---
+++ api/src/main/java/com/wm/workoutmonitoring/exceptions/GenderException.java
@@ -0,0 +1,7 @@
+package com.wm.workoutmonitoring.exceptions;
+
+public class GenderException extends Exception{
+    public GenderException(String exceptionMessage){
+        super(exceptionMessage);
+    }
+}

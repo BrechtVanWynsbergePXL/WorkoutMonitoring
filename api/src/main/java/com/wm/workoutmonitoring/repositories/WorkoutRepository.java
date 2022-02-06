b9e035065d9e23a56b9df7aefb530f1d76b29f63
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-06 18:09:27 +0100

    feat: added findAllByAccountId method

--- api/src/main/java/com/wm/workoutmonitoring/repositories/WorkoutRepository.java
+++ api/src/main/java/com/wm/workoutmonitoring/repositories/WorkoutRepository.java
@@ -4,9 +4,11 @@ import com.wm.workoutmonitoring.models.Workout;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WorkoutRepository extends CrudRepository<Workout, String> {
     List<Workout> findAll();
+    Optional<List<Workout>> findAllByAccountId(String id);
 
     @Override
     void deleteById(String id);

dd8247118893bd396ee0936b7e5867f44709ad9a
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-06 18:10:27 +0100

    feat: updated existing methods
    Used GenderService to determine Gender from gender enum
    Updated delete method to return a string instead of an int

--- api/src/main/java/com/wm/workoutmonitoring/services/AccountService.java
+++ api/src/main/java/com/wm/workoutmonitoring/services/AccountService.java
@@ -1,15 +1,18 @@
 package com.wm.workoutmonitoring.services;
 
 import com.wm.workoutmonitoring.dtos.AccountDTO;
+import com.wm.workoutmonitoring.exceptions.GenderException;
 import com.wm.workoutmonitoring.exceptions.PasswordException;
 import com.wm.workoutmonitoring.models.Account;
 import com.wm.workoutmonitoring.models.Gender;
 import com.wm.workoutmonitoring.repositories.AccountRepository;
+import com.wm.workoutmonitoring.services.helpers.GenderService;
 import com.wm.workoutmonitoring.services.helpers.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,6 +24,9 @@ public class AccountService {
 
     @Autowired
     private PasswordService passwordService;
+
+    @Autowired
+    private GenderService genderService;
 
     public List<Account> findAll() {
         return accountRepository.findAll();
@@ -42,13 +48,10 @@ public class AccountService {
         account.setEmail(accountDTO.getEmail());
         account.setDateOfBirth(accountDTO.getDateOfBirth());
 
-        if (accountDTO.getGender().equals("M") || accountDTO.getGender().equals("Male") ||
-                accountDTO.getGender().equals("m") || accountDTO.getGender().equals("male")) {
-            account.setGender(Gender.MALE);
-        } else if (accountDTO.getGender().equals("F") || accountDTO.getGender().equals("Female") ||
-                accountDTO.getGender().equals("f") || accountDTO.getGender().equals("female")) {
-            account.setGender(Gender.FEMALE);
-        } else {
+        try{
+            account.setGender(genderService.determineGender(accountDTO.getGender()));
+        } catch (GenderException e){
+            System.out.println(e.getMessage());
             account.setGender(Gender.UNDISCLOSED);
         }
 
@@ -58,11 +61,13 @@ public class AccountService {
             return null;
         }
 
-        return account;
+        account.setWorkoutList(new ArrayList<>());
+
+        return accountRepository.save(account);
     }
 
-    public int deleteAccount(String id){
+    public String deleteAccount(String id){
         accountRepository.deleteById(id);
-        return 200;
+        return "Account successfully deleted.";
     }
 }

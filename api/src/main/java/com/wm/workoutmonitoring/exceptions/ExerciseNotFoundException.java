977db09807a23c463090d974b18532363bfcd968
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-07 03:15:52 +0100

    feat: added exception class

---
+++ api/src/main/java/com/wm/workoutmonitoring/exceptions/ExerciseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.wm.workoutmonitoring.exceptions;
+
+public class ExerciseNotFoundException extends Exception{
+    public ExerciseNotFoundException(String exceptionMessage) {
+        super(exceptionMessage);
+    }
+}

bada25e48d91089a01c7ca480dce19230a9ca61e
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-07 03:17:12 +0100

    feat: added findByWorkoutId() method

--- api/src/main/java/com/wm/workoutmonitoring/repositories/ExerciseRepository.java
+++ api/src/main/java/com/wm/workoutmonitoring/repositories/ExerciseRepository.java
@@ -4,9 +4,11 @@ import com.wm.workoutmonitoring.models.Exercise;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExerciseRepository extends CrudRepository<Exercise, String> {
     List<Exercise> findAll();
+    Optional<List<Exercise>> findByWorkoutId(String id);
 
     @Override
     void deleteById(String id);

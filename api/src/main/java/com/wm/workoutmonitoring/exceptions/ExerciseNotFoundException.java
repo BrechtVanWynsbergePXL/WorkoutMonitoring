94fce31f415a3c4b57a996f12bbf77483ce902b9
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-14 23:26:38 +0100

    fix: fix formatting exceptions

--- api/src/main/java/com/wm/workoutmonitoring/exceptions/ExerciseNotFoundException.java
+++ api/src/main/java/com/wm/workoutmonitoring/exceptions/ExerciseNotFoundException.java
@@ -1,6 +1,6 @@
 package com.wm.workoutmonitoring.exceptions;
 
-public class ExerciseNotFoundException extends Exception{
+public class ExerciseNotFoundException extends Exception {
     public ExerciseNotFoundException(String exceptionMessage) {
         super(exceptionMessage);
     }
--- api/src/main/java/com/wm/workoutmonitoring/exceptions/WorkoutNotFoundException.java
+++ api/src/main/java/com/wm/workoutmonitoring/exceptions/WorkoutNotFoundException.java
@@ -1,6 +1,6 @@
 package com.wm.workoutmonitoring.exceptions;
 
-public class WorkoutNotFoundException extends Exception{
+public class WorkoutNotFoundException extends Exception {
     public WorkoutNotFoundException(String exceptionMessage) {
         super(exceptionMessage);
     }

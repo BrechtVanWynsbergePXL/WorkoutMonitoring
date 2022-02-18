c4b7889e0e127b96fcb034b6a1db15f2f8cda94b
BrechtVanWynsbergePXL <brecht.vanwynsberge@student.pxl.be>
2022-02-18 03:14:36 +0100

    fix: simplified GenderService

--- api/src/main/java/com/wm/workoutmonitoring/services/helpers/GenderService.java
+++ api/src/main/java/com/wm/workoutmonitoring/services/helpers/GenderService.java
@@ -1,20 +1,15 @@
 package com.wm.workoutmonitoring.services.helpers;
 
-import com.wm.workoutmonitoring.exceptions.GenderException;
 import com.wm.workoutmonitoring.models.Gender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GenderService {
     public Gender determineGender(String gender) {
-        switch (gender) {
-            case "M":
-            case "Male":
+        switch (gender.toLowerCase()) {
             case "m":
             case "male":
                 return Gender.MALE;
-            case "F":
-            case "Female":
             case "f":
             case "female":
                 return Gender.FEMALE;
